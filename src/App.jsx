ab8579a8813c1a2474cf28f8e8a289dc8effdea7
aldoprogrammer <aldobesma@gmail.com>
2024-03-01 00:06:24 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import Sidebar from './components/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <>
-
     <Sidebar />
    </>
   )
